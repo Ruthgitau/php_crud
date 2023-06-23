2383da64ecde6272b620a63d4d3bad594ee5f11c
Ruthgitau <gitaur72@gmail.com>
2023-06-23 13:00:55 +0300

    test

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+$server_name= "local host";
+$username="username";
+$password="password";
+
+//Creating connection
+$conn = new mysqli($server_name, $username, $password);
+
+//checking connection
+if($conn ->connect_error){
+    die("Connection Failed:" . $conn->connect_error);
+}
+echo "Connected Successfully";
+?>
+                                                                                                                                                                                                              - vgchhhhhhhhhhZA~S;p,.lk mj
